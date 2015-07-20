07a354f323d0a8b20a4ee617a350e1d13908f76a
Josh Metry <josh.metry@fresno.edu>
2015-07-19 20:26:59 -0700

    final commit for this project

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,44 @@
-var arr = [];
-var ask = prompt("please enter a string.");
-arr.push(ask);
+var colors = ['Black',
+'Silver',
+'Gray',
+'Maroon',
+'Red',
+'Blue',
+'Purple',
+'Fuchsia',
+'Green',
+'Lime',
+'Olive',
+'Yello',
+'Navy',
+'Teal',
+'Aqua'
+];
+var font = 0;
+var bg = 0;
+
+function addTextToPage()
+{
+	do{
+    	font = Math.floor((Math.random() * 15));
+		bg = Math.floor((Math.random() * 15));
+	} while (font == bg);
+
+	var ask = prompt("please enter a string.");
+	
+    while(!ask || typeof(ask) !== "string")
+    {
+        alert("You need to enter a string in the textbox provided.");
+        ask = prompt("please enter a string.");
+    }
+	
+	var leftDiv = document.createElement("div");
+	p = document.createElement('p');
+	p.innerHTML = '<p style="background:' + colors[bg] + ';color:' + colors[font] + '; font-size:20px;"> ' + ask + '</p>';
+	leftDiv.appendChild(p); // Add name to left div
+
+	document.getElementById("updateDiv").appendChild(leftDiv);
+}
 
 
 
